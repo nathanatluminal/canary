8026a8751284af2aef1c20826069c1f7d931ec72
Nathan McCourtney <beaknit@gmail.com>
2015-03-25 16:25:29 -0400

    Move args to parameterized, Fix tabs in bin

--- lib/canary.py
+++ lib/canary.py
@@ -7,28 +7,27 @@ import time
 import urllib.request
 
 
-
-#TODO:  Parameterize inputs
-IP_ADDRESS = '127.0.0.1'
-EC2_REGION = 'us-west-2'
-
 LOG = logging.getLogger()
 LOG_FORMAT = '%(asctime)s - %(threadName)s - %(name)s - %(levelname)s - %(message)s'
 logging.basicConfig(format=LOG_FORMAT,level=logging.DEBUG, filename='/tmp/canary.log')
 
-def main():
+def main(args):
+    ip_address = args.ip_address
+    ec2_region = args.region
+    role_name = args.role_name
+    
     logging.info("Beginning poll")
     try:
         while True:
-            logging.info("Reading metadata from http://%s/latest/meta-data/" % IP_ADDRESS)
-            meta_data = ['iam/security-credentials/fugue-conductor-iam-role']
+            logging.info("Reading metadata from http://%s/latest/meta-data/" % ip_address)
+            meta_data = ['iam/security-credentials/%s' % role_name]
             for datum in meta_data:
-                req = urllib.request.Request('http://%s/latest/meta-data/%s' % (IP_ADDRESS,datum))
+                req = urllib.request.Request('http://%s/latest/meta-data/%s' % (ip_address,datum))
                 response = urllib.request.urlopen(req)
                 payload = response.read()
                 logging.info(payload)
 
-            ec2_conn = boto.ec2.connect_to_region(EC2_REGION)
+            ec2_conn = boto.ec2.connect_to_region(ec2_region)
             reservations = ec2_conn.get_all_reservations()
             instance_ids_by_reservation = [[instance.id for instance in reservation.instances] for reservation in reservations]
             logging.info("INSTANCES BY RESERVATION --> %s" % str(instance_ids_by_reservation))
